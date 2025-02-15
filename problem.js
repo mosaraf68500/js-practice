5aac5e9f280a13f2d1bd7d2d67de552b68a4b125
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-02-15 12:08:57 +0600

    code

--- problem.js
+++ problem.js
@@ -1,1 +1,14 @@
-const tolarest=[12,45,46,87,56,43]
+const numbers=[87,23,123,56,87,98];
+
+function max(numbers){
+    let maxValue=numbers[0];
+    for(const num of numbers){
+        if(num>maxValue){
+            maxValue=num;
+        }
+    }
+    return maxValue
+}
+
+const maxNumber=max(numbers);
+console.log(maxNumber)
